089abe5b79e4d55fe86579a15d15af9e6a54b3f0
koss-sh <shilinskii22@gmail.com>
2022-12-19 21:27:56 +0300

    task 7 разница в весе

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,9 @@ public class Main {
     public static void main(String[] args) {
         var firstBoxerWeight = 78.2;
         var secondBoxerWeight = 82.7;
-        var sumWeight = firstBoxerWeight + secondBoxerWeight;
-        System.out.println("Общий вес боксеров равен " + sumWeight + " кг.");
-        var differenceWeight = secondBoxerWeight - firstBoxerWeight;
-        System.out.println("Разница в весе 2 боксеров равна  " + differenceWeight + " кг.");
+        var differenceWeight1 = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Первый способ: Разница в весе 2 боксеров равна  " + differenceWeight1 + " кг.");
+        var differenceWeight2 = secondBoxerWeight % firstBoxerWeight;
+        System.out.println("Второй способ: Разница в весе 2 боксеров равна  " + differenceWeight2 + " кг.");
     }
 }
