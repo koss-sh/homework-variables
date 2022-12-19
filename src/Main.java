933875da079dfdcd8af56d19e86a7625e8cdbaba
koss-sh <shilinskii22@gmail.com>
2022-12-19 21:21:04 +0300

    task 6 вес боксеров

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        var frog = 3.5;
-        System.out.println(frog);
-        frog = frog * 10;
-        System.out.println(frog);
-        frog = frog / 3.5;
-        System.out.println(frog);
-        frog = frog + 4;
-        System.out.println(frog);
+        var firstBoxerWeight = 78.2;
+        var secondBoxerWeight = 82.7;
+        var sumWeight = firstBoxerWeight + secondBoxerWeight;
+        System.out.println("Общий вес боксеров равен " + sumWeight + " кг.");
+        var differenceWeight = secondBoxerWeight - firstBoxerWeight;
+        System.out.println("Разница в весе 2 боксеров равна  " + differenceWeight + " кг.");
     }
 }
